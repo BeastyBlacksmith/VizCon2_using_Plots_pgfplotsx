def6ce3bc1f947f940197fee8ab862ed7733a9c3
Simon Christ <SimonChrist@gmx.de>
2020-03-06 00:00:52 +0100

    add contour3d example

--- slides.jl
+++ slides.jl
@@ -9,20 +9,30 @@
 #----------------------------------------
 # @__{slide()}__
 #
-# ## Why? 
+# ## Why?
 #
 # - Missing features
 # - (Hopefully) easier to maintain
 #----------------------------------------
 # @__{slide()}__
-# 
+#
 # ## What's new?
 #----------------------------------------
 # @__{slide()}__
-# 
+#
+# ### 3D contours
+using Plots;
+pgfplotsx();
+f(x, y) = exp(-x^2 - y^2)
+xs = range(-1, 1, length = 20)
+ys = range(-1, 1, length = 20)
+contour3d(xs, ys, [f(x, y) for x in xs, y in ys])
+#----------------------------------------
+# @__{slide()}__
+#
 # ## What's missing?
 #----------------------------------------
 # @__{slide()}__
-# 
+#
 # ## Lessons learned
 #----------------------------------------
